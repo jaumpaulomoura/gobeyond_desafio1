8ba363f21b380aec3d3c9204c2a9656e91bcdea1
Joao Paulo <joaopaulo92803693@gmail.com>
2021-09-11 19:53:27 -0300

    Responsivo

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,16 @@ $(window).ready(function () {
         nextArrow: '<button type="button" class="slick-next"><svg width="14" height="24" viewBox="0 0 14 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.28664 23.3133L13.6 12L2.28664 0.686657L0.399977 2.57199L9.82931 12L0.399977 21.428L2.28664 23.3133Z" fill="#F2F2F2"/></svg></button>'
     });
 
+    $('.main-banner-mobile').slick({
+      dots: true,
+      controls: true,
+      autoplay: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+  });
+
     $('.products ul').slick({
         infinite: true,
         speed: 300,
